225aa695ce8db711918c6c5784ec9d35b09fa859
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-02-03 18:41:14 +0100

    menu, product grid

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,7 +1,14 @@
+import { TopMenu } from '@/components';
+
 export default function ShopLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return <main className="bg-red-500 min-h-screen">{children}</main>
+  return (
+    <main className="min-h-screen">
+      <TopMenu />
+      <div className="px-0 sm:px-7">{children}</div>
+    </main>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from 'next'
-import { inter } from '../config/fonts'
+import type { Metadata } from 'next';
+import { inter } from '../config/fonts';
 
-import './globals.css'
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Teslo | Shop',
+  title: 'Navas | Shop',
   description: 'A Virtual Store',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
