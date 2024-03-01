56edab9cff20e2b07a081fbfed38a2956c59d625
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-03-01 10:00:52 +0100

    auth nextjs

---
+++ src/auth.config.ts
@@ -0,0 +1,39 @@
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+export const authConfig: NextAuthConfig = {
+  pages: {
+    signIn: '/auth/login',
+    newUser: '/auth/new-account',
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        console.log(parsedCredentials.success);
+
+        if (!parsedCredentials.success) {
+          return null;
+        }
+
+        const { email, password } = parsedCredentials.data;
+
+        console.log({ email, password });
+
+        //Search email
+
+        //Match password
+
+        // Return user
+
+        return null;
+      },
+    }),
+  ],
+};
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
